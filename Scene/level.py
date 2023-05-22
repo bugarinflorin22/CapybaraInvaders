2466b3f1db15a45fc19421a34a6658b935d5618d
Adamut Alexandra <alexandra.adamut56@gmail.com>
2023-05-22 19:11:48 +0300

    schimbare

--- Scene/level.py
+++ Scene/level.py
@@ -4,7 +4,7 @@ import random
 
 
 # Player
-player_Img = pygame.image.load("Art/background.png")  # the player icon
+player_Img = pygame.image.load("../Art/background.png")  # the player icon
 playerX = 430  # the coordonates where we want the player to be on the weight (x axis)
 playerY = 590  # the coordonates where we want the player to be on the height (y axis)
 playerX_change = 0
@@ -17,7 +17,7 @@ enemyX_change = []
 enemyY_change = []
 number_enemies = 6
 for i in range(number_enemies):
-    enemy_Img.append(pygame.image.load("Art/level/enemy.png"))
+    enemy_Img.append(pygame.image.load("../Art/level/enemy.png"))
     enemyX.append(random.randint(0, 835))
     enemyY.append(random.randint(57, 130))
     enemyX_change.append(0.3)

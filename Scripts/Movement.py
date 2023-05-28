1352af59bf55bbe532c16f399eb1848d59cadf09
Adamut Alexandra <alexandra.adamut56@gmail.com>
2023-05-28 18:54:24 +0300

    movement player

--- Scripts/Movement.py
+++ Scripts/Movement.py
@@ -5,13 +5,13 @@ class Player_Movement:
     def __init__(self, event, player):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player.speed_x = -7
+                player.speed_x = -10
             elif event.key == pygame.K_RIGHT:
-                player.speed_x = 7
+                player.speed_x = 10
             elif event.key == pygame.K_UP:
-                player.speed_y = -5
+                player.speed_y = -7
             elif event.key == pygame.K_DOWN:
-                player.speed_y = 5
+                player.speed_y = 7
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
